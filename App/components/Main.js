e8af5e6c38c717593cc2bfd6505f46f725076418
serhii <sergeyromanenko911@gmail.com>
2018-04-10 01:31:35 +0300

    add componentsMain

---
+++ App/components/Main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { 
+    StyleSheet, 
+    Text, 
+    View,
+    TextInput,
+    ScrollView,
+    TouchableOpacity
+} from 'react-native';
+
+export default class Main extends React.Component {
+  render() {
+    return (
+      <View>
+          <Text>Testing</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+  },
+});
